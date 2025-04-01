9614daed79a5edf5d6e841ec31d315050429d4de
joaovitorsg011 <joaovitorsimao13ff@gmail.com>
2025-04-01 14:15:31 -0300

    Implementando Docker e Apache

---
+++ src/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bem-vindo!</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            text-align: center;
+            margin-top: 50px;
+            background-color: #ecee8e;
+        }
+        h1 {
+            color: #4CAF50;
+        }
+        .btn-login {
+            background-color: #1b631d;
+            color: white;
+            padding: 10px 20px;
+            font-size: 16px;
+            border: none;
+            border-radius: 5px;
+            cursor: pointer;
+            text-decoration: none;
+        }
+        .btn-login:hover {
+            background-color: #1b631d;
+        }
+    </style>
+</head>
+<body>
+
+    <h1>Bem Vindo a nossa Pagina</h1>
+    <p>Para continuar, por favor faça o login</p>
+    <a href="login.php" class="btn-login">Ir para Login</a>
+
+</body>
+</html>
