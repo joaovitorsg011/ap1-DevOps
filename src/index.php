a8689996bacf9216adb5b036bb0afc38e8ed3e79
murylo <muryloalves15@gmail.com>
2025-04-01 23:36:15 -0300

    tem que separar joao &*$#@

--- src/index.php
+++ src/index.php
@@ -4,30 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bem-vindo!</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            text-align: center;
-            margin-top: 50px;
-            background-color: #ecee8e;
-        }
-        h1 {
-            color: #4CAF50;
-        }
-        .btn-login {
-            background-color: #1b631d;
-            color: white;
-            padding: 10px 20px;
-            font-size: 16px;
-            border: none;
-            border-radius: 5px;
-            cursor: pointer;
-            text-decoration: none;
-        }
-        .btn-login:hover {
-            background-color: #1b631d;
-        }
-    </style>
 </head>
 <body>
 
